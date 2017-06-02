55fb51e8d7883b70c9388ae7e8119081244f2a4c
Gerdriaan Mulder <mrngm@moeilijklastig.nl>
2017-06-02 21:11:23 +0200

    Added descriptions and pretty printer

--- src/extract.py
+++ src/extract.py
@@ -38,7 +38,12 @@ def parse_advisory(advisory):
         "datum":       r"^Uitgiftedatum\s+:(?P<datum>.*)(?P<datumrest>(\n +.*)+)?",
         "toepassing":  r"^Toepassing\s+:(?P<toepassing>.*)(?P<toepassingrest>(\n +.*)+)?",
         "versie":      r"^Versie\(s\)\s+:(?P<versie>.*)(?P<versierest>(\n +.*)+)?",
-        "platform":    r"^Platform\(s\)\s+:(?P<platform>.*)(?P<platformrest>(\n +.*)*)?"
+        "platform":    r"^Platform\(s\)\s+:(?P<platform>.*)(?P<platformrest>(\n +.*)*)?",
+        "samenvatting":r"^Samenvatting\s*\n(?P<samenvatting>\s+.*)(?P<samenvattingrest>(\n +.*)*)?",
+        "beschrijving":r"^Beschrijving\s*\n(?P<beschrijving>\s+.*)(?P<beschrijvingrest>(\n +.*)*)?",
+        "solution":    r"^Mogelijke oplossingen\s*\n(?P<solution>\s+.*)(?P<solutionrest>(\n +.*)*)?",
+        "disclaimer":  r"^Vrijwaringsverklaring\s*\n(?P<disclaimer>\s+.*)(?P<disclaimerrest>(\n +.*)*)?"
+
     }
 
     for index in list(regexes.keys()):
@@ -51,9 +56,11 @@ def parse_advisory(advisory):
 
     return parsed_advisory
 
-advisory_dict = parse_advisory(extract_advisory("./testpagina.html"))
+def pretty_print(parsed_advisory):
+    kans   = parsed_advisory['kans'][0:1].upper()
+    schade = parsed_advisory['schade'][0:1].upper()
+    print("{}/{}, {}: {}".format(kans, schade, parsed_advisory['advisoryid'], parsed_advisory['titel']))
 
-for t in sorted(advisory_dict.keys()):
-    print("{}: {}".format(t, advisory_dict[t]))
+pretty_print(parse_advisory(extract_advisory("./testpagina.html")))
 
 # vim: set et:ts=4:sw=4:
